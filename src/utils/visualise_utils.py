7ad2ab8917b40b627ed091050439540cbfb0c5da
Huw Cheston <hwc31@cam.ac.uk>
2023-09-06 13:45:08 +0100

    Started adding some visualisation code

---
+++ src/utils/visualise_utils.py
@@ -0,0 +1,84 @@
+import functools
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+import src.utils.analyse_utils as autils
+
+# Ignore annoying matplotlib INFO warnings created even though I'm doing nothing wrong
+plt.set_loglevel('WARNING')
+
+# Define constants
+WIDTH = 18.8
+FONTSIZE = 18
+
+ALPHA = 0.4
+BLACK = '#000000'
+WHITE = '#FFFFFF'
+RED = '#FF0000'
+
+N_BOOT = 10000
+
+def plot_decorator(plotter: callable):
+    """
+    Decorator applied to any plotting function.
+    Used to create a folder, save plot into this, then close it cleanly and exit.
+    """
+
+    @functools.wraps(plotter)
+    def wrapper(*args, **kwargs):
+        # Define the filetypes we want to save the plot as
+        filetypes = ['png', 'svg']
+        # Create the output directory to store the plot
+        output = kwargs.get('output_dir', None)
+        # If we're accessing this decorator from a class, need to get the output by accessing the class attributes
+        if output is None:
+            output = args[0].output_dir  # Will be None anyway if no output_dir ever passed to class
+        # Create the plot and return the figure
+        fig, fname = plotter(*args, **kwargs)
+        # If we've provided an output directory, create a folder and save the plot within it
+        if output is not None:
+            # Iterate through all filetypes and save the plot as each type
+            for filetype in filetypes:
+                fig.savefig(f'{fname}.{filetype}', format=filetype, facecolor=WHITE)
+    return wrapper
+
+
+def create_output_folder(out):
+    """
+    Create a folder to store the plots, with optional subdirectory. Out should be a full system path.
+    """
+    Path(out).mkdir(parents=True, exist_ok=True)
+    return out
+
+
+class BasePlot:
+    """
+    Base plotting class from which others inherit
+    """
+    output_dir = fr'{autils.get_project_root()}\reports\figures'
+    df = None
+    fig, ax = None, None
+    g = None
+
+    def __init__(self, **kwargs):
+        # Set fontsize
+        plt.rcParams.update({'font.size': FONTSIZE})
+        self.figure_title = kwargs.get('figure_title', 'baseplot')
+
+    @plot_decorator
+    def create_plot(self):
+        self._create_plot()
+        self._format_ax()
+        self._format_fig()
+        fname = rf'{self.output_dir}\{self.figure_title}'
+        return self.fig, fname
+
+    def _create_plot(self):
+        return
+
+    def _format_ax(self):
+        return
+
+    def _format_fig(self):
+        return
--- src/visualise/visualize.py
+++ src/visualise/visualize.py
@@ -1,5 +1,68 @@
-# import seaborn as sns
-# import matplotlib.pyplot as plt
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import src.utils.analyse_utils as autils
+import src.utils.visualise_utils as vutils
+
+
+class ScatterPlotByBeat(vutils.BasePlot):
+    def __init__(self, item, **kwargs):
+        self.item = item
+        super().__init__(figure_title=rf'onsets_plots\scatterplot_bybeat_{self.item.item["fname"]}', **kwargs)
+        self.df = pd.DataFrame(self.format_df())
+        self.cmap = sns.color_palette(
+            kwargs.get('cmap', 'husl'),
+            n_colors=len(autils.INSTRS_TO_PERF.keys()),
+            as_cmap=False
+        )
+        self.fig, self.ax = plt.subplots(
+            nrows=1,
+            ncols=len(autils.INSTRS_TO_PERF.keys()),
+            sharex=True,
+            sharey=True,
+            figsize=(vutils.WIDTH, 7)
+        )
+
+    def format_df(self):
+        for instr in autils.INSTRS_TO_PERF.keys():
+            instr_ons = self.item.ons[instr]
+            z = zip(self.item.ons['mix_madmom'], self.item.ons['mix_madmom'][1:], self.item.ons['mix_beatpositions'])
+            for beat1, beat2, beat1pos in z:
+                vals = instr_ons[np.logical_and(beat1 <= instr_ons, instr_ons < beat2)]
+                for i in vals:
+                    pos = ((i - beat1) / (beat2 - beat1)) + beat1pos
+                    yield {
+                        'instrument': instr,
+                        'timestamp': pd.to_datetime(datetime.fromtimestamp(beat1).strftime('%H:%M:%S')),
+                        'musical_position': pos if pos < 4.9 else pos - 4
+                    }
+
+    def _create_plot(self):
+        for ax, (idx, grp), col in zip(self.ax.flatten(), self.df.groupby('instrument'), self.cmap):
+            g = sns.scatterplot(data=grp, x='musical_position', y='timestamp', ax=ax, color=col, s=40, legend=None)
+            g.set_title(idx.title())
+
+    def _format_ax(self):
+        minor_ticks = [i + f for i in range(1, 5) for f in (1 / 3, 2 / 3)]
+        for ax in self.ax.flatten():
+            ax.set(xlim=(0.8, 5.2), xticks=[1, 2, 3, 4], xlabel='', ylabel='')
+            ax.set_xticks(minor_ticks, labels=[], minor=True)
+            ax.grid(which='major', ls='-', lw=1)
+            ax.grid(which='minor', ls='--', lw=0.3)
+            plt.setp(ax.spines.values(), linewidth=2)
+            ax.tick_params(axis='both', width=2)
+
+    def _format_fig(self):
+        self.fig.supxlabel(r'Beat ($\frac{1}{4}$ note)')
+        self.fig.supylabel('Time (s)')
+        self.fig.suptitle(self.item.item['fname'])
+        self.fig.subplots_adjust(left=0.11, right=0.95, top=0.85, bottom=0.15)
+
+
 #
 # plt.rcParams.update({'font.size': 18})
 # def f(row):
@@ -36,4 +99,64 @@
 # fig.supxlabel('Influenced')
 # fig.supylabel('Coupling coefficient')
 # fig.subplots_adjust(left=0.075, right=0.9, top=0.85, bottom=0.15)
+# plt.show()
+
+
+
+
+# # Plotting parameter optimization results sketches
+# import pandas as pd
+# import numpy as np
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+#
+# import src.utils.analyse_utils as autils
+#
+# plt.rcParams.update({'font.size': 18})
+#
+# csvs = []
+# for instr in autils.INSTRS_TO_PERF.keys():
+#     csvs.append(pd.DataFrame(autils.load_csv(rf'{autils.get_project_root()}\references\parameter_optimisation', f'onset_detect_{instr}')))
+# csvs.append(pd.DataFrame(autils.load_csv(rf'{autils.get_project_root()}\references\parameter_optimisation', f'beat_track_mix')))
+# df = pd.concat(csvs).reset_index(drop=True)
+#
+# for idx, grp in df.groupby(['instrument', 'iterations']):
+#     assert len(grp) == 21
+#     assert grp['fname'].duplicated().any() == False
+#
+# df['instrument'] = df['instrument'].str.title()
+# fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(18.8, 5), sharex=False, sharey=True)
+# g = sns.lineplot(
+#     data=df, x='iterations', y='f_score', hue='instrument', errorbar='sd', lw=3,
+#     estimator=np.mean, err_kws=dict(alpha=0.2), n_boot=1000, seed=1, ax=ax[0],
+#     hue_order=['Piano', 'Bass', 'Drums', 'Mix']
+# )
+# g.set(xlabel='Optimization iterations', ylabel='Objective function ($F$)', xlim=(-5, 500))
+# g.axhline(y=1, xmin=g.get_xlim()[0], xmax=g.get_xlim()[1], color='black', alpha=0.3, ls='--', lw=3)
+# sns.move_legend(g, loc='lower right', title='Instrument')
+# ax[0].tick_params(width=3, )
+# plt.setp(ax[0].spines.values(), linewidth=2)
+#
+#
+# mima = df.groupby(['instrument', 'mbz_id']).agg({'f_score': ['min', 'max']})
+# mima.columns = mima.columns.droplevel()
+# opt = df[df.groupby(['instrument'])['iterations'].transform(max) == df['iterations']].groupby(['instrument', 'mbz_id'])['f_score'].mean()
+# combined = pd.concat([mima, opt], axis=1).rename(columns=dict(min='Worst run', max='Best run', f_score='Optimized')).reset_index(drop=False).melt(id_vars=['instrument', 'mbz_id'], var_name='Parameters')
+#
+# g = sns.barplot(
+#     data=combined, x='Parameters', y='value', hue='instrument', estimator=np.mean,
+#     errorbar=('ci', 95), ax=ax[1], lw=2, width=0.8, errcolor='black', errwidth=3,
+#     n_boot=1000, seed=1, capsize=0.075, edgecolor='black', hue_order=['Piano', 'Bass', 'Drums', 'Mix']
+# )
+# g = sns.stripplot(
+#     data=combined, x='Parameters', y='value', hue='instrument', ax=ax[1], alpha=0.5,
+#     dodge=True, palette='dark:black',legend=False, s=6, marker='.', jitter=0.1, hue_order=['Piano', 'Bass', 'Drums', 'Mix']
+# )
+# g.set(xlabel='', ylabel='')
+# sns.move_legend(g, loc='best', title='Instrument')
+# ax[1].tick_params(width=3, )
+# plt.setp(ax[1].spines.values(), linewidth=2)
+# fig.suptitle('Optimizing Bill Evans corpus ($N$=21) with NLopt "Sbplx" algorithm')
+# fig.subplots_adjust(top=0.85, bottom=0.15, right=0.975, left=0.05, wspace=0.1)
+#
 # plt.show()
