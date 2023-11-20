7a828312ffe3134bdcceebaa5ec4ec0457504c3e
Huw Cheston <hwc31@cam.ac.uk>
2023-11-20 18:02:47 +0000

    Updated asynchrony notebook and plots

--- src/visualise/asynchrony_plots.py
+++ src/visualise/asynchrony_plots.py
@@ -3,6 +3,8 @@
 
 """Classes used for plotting asynchrony and rhythmic feel"""
 
+import warnings
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,11 +12,16 @@ import pandas as pd
 import scipy.signal as signal
 import scipy.stats as stats
 import seaborn as sns
+import statsmodels.formula.api as smf
+from statsmodels.tools.sm_exceptions import ConvergenceWarning
 
 import src.visualise.visualise_utils as vutils
 from src import utils
 
-__all__ = ['PolarPlotAsynchrony', 'BarPlotProportionalAsynchrony', 'HistPlotProportionalAsynchrony']
+__all__ = [
+    'PolarPlotAsynchrony', 'BarPlotProportionalAsynchrony', 'HistPlotProportionalAsynchrony',
+    'RegPlotPianistAsynchrony'
+]
 
 
 class PolarPlotAsynchrony(vutils.BasePlot):
@@ -284,3 +291,227 @@ class HistPlotProportionalAsynchrony(vutils.BasePlot):
 
     def _format_fig(self):
         self.fig.subplots_adjust(left=0.1, right=0.95, top=0.85, bottom=0.175)
+
+
+class RegPlotPianistAsynchrony(vutils.BasePlot):
+    # Disable convergence and user warnings here, raised when the model is created with bootstrapping
+    warnings.simplefilter('ignore', ConvergenceWarning)
+    warnings.simplefilter('ignore', UserWarning)
+
+    FORMULA = "diff ~ tempo_standard * C(instr, Treatment(reference='bass'))"
+    RE_STRUCT = "0 + tempo_standard + C(instr, Treatment(reference='bass'))"
+    N_BOOT = 10000
+    BIN_MULTIPLER = 1.5
+
+    # These are keywords that we pass into our given plot types
+    LINE_KWS = dict(lw=vutils.LINEWIDTH * 2, ls=vutils.LINESTYLE, zorder=5)
+    FILL_KWS = dict(lw=0, ls=vutils.LINESTYLE, alpha=vutils.ALPHA, zorder=5)
+    SCATTER_KWS = dict(
+        hue_order=list(utils.INSTRUMENTS_TO_PERFORMER_ROLES.keys())[1:],
+        palette=vutils.RGB[1:], markers=['s', 'D'], s=40,
+        edgecolor=vutils.BLACK, zorder=3, alpha=vutils.ALPHA * 2,
+    )
+    HIST_KWS = dict(
+        kde=False, color=vutils.BLACK, alpha=vutils.ALPHA,
+        lw=vutils.LINEWIDTH, ls=vutils.LINESTYLE
+    )
+
+    def __init__(self, async_df: pd.DataFrame, **kwargs):
+        """Called when initialising the class"""
+        self.corpus_title = 'corpus_chronology'
+        # Initialise the base plot with our given kwargs
+        super().__init__(figure_title=fr'asynchrony_plots\regplot_pianistasync_{self.corpus_title}', **kwargs)
+        self.df = async_df
+        self.md = self._mixedlm(self.df)
+        self.fig, self.ax = plt.subplots(
+            nrows=2, ncols=2, figsize=(vutils.WIDTH, vutils.WIDTH / 2),
+            gridspec_kw=dict(width_ratios=(11, 1), height_ratios=(1, 5)),
+        )
+        # The main ax for plotting the regression/scatter plot
+        self.main_ax = self.ax[1, 0]
+        # Marginal ax, for plotting histograms
+        self.marginal_ax = np.array([self.ax[0, 0], self.ax[1, 1]])
+        # Top right corner ax, which we can go ahead and disable
+        self.ax[0, 1].axis('off')
+
+    def _mixedlm(self, data):
+        return smf.mixedlm(
+            self.FORMULA,
+            data=data,
+            groups=data['bandleader'],
+            re_formula=self.RE_STRUCT
+        ).fit()
+
+    def _get_line(self, md):
+        # Get our intercept and tempo parameters from the model
+        intercept = md.params['Intercept']
+        tempo = md.params['tempo_standard']
+        # Get our bass parameters from the model
+        is_bass = 0
+        is_bass_tempo = 0
+        # Get our drums parameters from the model
+        is_drums = md.params["C(instr, Treatment(reference='bass'))[T.drums]"]
+        is_drums_tempo = md.params["tempo_standard:C(instr, Treatment(reference='bass'))[T.drums]"]
+        # This is the range of values we'll be iterating through, taken from the actual results
+        low_ = int(np.floor(self.df['tempo'].min()))
+        high_ = int(np.ceil(self.df['tempo'].max()))
+        mean_, std_ = np.array(range(low_, high_)).mean(), np.array(range(low_, high_)).std()
+        # Iterate through each BPM in our range
+        for bpm in range(low_, high_):
+            print(bpm)
+            # Standardise the BPM (Z-score) according to the observed values
+            bpm_z = (bpm - mean_) / std_
+            tempo_coeff = tempo * bpm_z
+            # Iterate through each instrument and both coefficients
+            for instr_, coeff_, interact_ in zip(
+                    ['bass', 'drums'], [is_bass, is_drums], [is_bass_tempo, is_drums_tempo]
+            ):
+                # Construct the BUR value by following the regression equation
+                diff = intercept + tempo_coeff + coeff_ + (interact_ * bpm_z)
+                # Yield a dictionary of the results
+                yield dict(tempo=bpm, tempo_std=bpm_z, instr=instr_, diff=diff)
+
+    def _get_bootstrapped_sample(self):
+        """Returns bootstrapped samples of the full dataset"""
+        def bootstrap(state: int):
+            """Bootstrapping function"""
+            # Take a random sample of bandleaders and iterate through each
+            for _, leader in bandleaders.sample(frac=1, replace=True, random_state=state).items():
+                # Get all the data belonging to each bandleader
+                yield self.df[self.df['bandleader'] == leader]
+
+        # These are the names of all bandleaders
+        bandleaders = pd.Series(self.df['bandleader'].unique())
+        for i in range(self.N_BOOT):
+            # Print the current iteration to act as a log
+            print(i)
+            # Return each bootstrapped sample as a single dataframe
+            yield pd.concat(bootstrap(i), axis=0)
+
+    def _format_bootstrap_lines(self, boot_models: list):
+        """Formats data from a series of bootstrapped models into one dataframe of errors"""
+        # Get a straight line for each bootstrapped model and combine into one dataframe
+        big = pd.concat([pd.DataFrame(self._get_line(boot)) for boot in boot_models], axis=1)
+        # Iterate through each tempo value
+        for idx, row in big.iterrows():
+            # Return a dictionary of results
+            yield dict(
+                tempo=row['tempo'].iloc[0],
+                instr=row['instr'].iloc[0],
+                low_ci=np.percentile(row['diff'], 2.5),
+                high_ci=np.percentile(row['diff'], 97.5)
+            )
+
+    def _create_main_plot(self):
+        # Get the line for the actual data
+        line_df = pd.DataFrame(self._get_line(self.md))
+        # Bootstrap to get random samples, replacement unit is bandleader
+        boot_samples = self._get_bootstrapped_sample()
+        # Create model for each sample of data
+        boot_mds = [self._mixedlm(sample) for sample in boot_samples]
+        # Convert all bootstrapped models into one single dataframe of errors
+        boot_lines = pd.DataFrame(self._format_bootstrap_lines(boot_mds))
+        # Iterate through each instrument and line color
+        for instr_, col_ in zip(['bass', 'drums'], vutils.RGB[1:]):
+            # First temporary dataframe: our actual data for this instrument
+            temp_ = line_df[line_df['instr'] == instr_]
+            # Plot the actual data
+            self.main_ax.plot(temp_['tempo'], temp_['diff'], color=col_, **self.LINE_KWS)
+            # Second temporary dataframe: our bootstrapped data for this instrument
+            temp_boot_ = boot_lines[boot_lines['instr'] == instr_]
+            # Fill between the low and high bounds
+            self.main_ax.fill_between(
+                temp_boot_['tempo'], temp_boot_['low_ci'], temp_boot_['high_ci'], color=col_, **self.FILL_KWS
+            )
+        sns.scatterplot(
+            data=self.df, x='tempo', y='diff', hue='instr', style='instr', ax=self.main_ax, **self.SCATTER_KWS
+        )
+
+    def _create_marginal_plots(self):
+        """Plots histograms and density estimates onto the marginal axis"""
+        # Top marginal plot
+        sns.histplot(
+            data=self.df, x='tempo', ax=self.marginal_ax[0],
+            bins=int(vutils.N_BINS * self.BIN_MULTIPLER),  **self.HIST_KWS
+        )
+        # Right marginal plot
+        sns.histplot(
+            data=self.df, y='diff', ax=self.marginal_ax[1],
+            bins=int(vutils.N_BINS / self.BIN_MULTIPLER),  **self.HIST_KWS
+        )
+
+    def _create_plot(self):
+        self._create_main_plot()
+        self._create_marginal_plots()
+
+    def _format_main_ax(self):
+        # Add a grid onto the plot
+        self.main_ax.grid(visible=True, axis='both', which='major', zorder=0, **vutils.GRID_KWS)
+        # Get our legend handles, and set their edge color to black
+        hand, _ = self.main_ax.get_legend_handles_labels()
+        for ha in hand:
+            ha.set_edgecolor(vutils.BLACK)
+        # Remove the old legend, then add the new one on
+        self.main_ax.get_legend().remove()
+        self.main_ax.legend(
+            hand, ['Bass', 'Drums'], loc='upper left', title='Instrument', frameon=True, framealpha=1,
+            edgecolor=vutils.BLACK
+        )
+        # Final attributes to set here
+        self.main_ax.set(
+            xticks=[100, 150, 200, 250, 300], yticks=[-1/128, 0, 1/128, 1/64, 1/32], xlim=(100, 315),
+            yticklabels=[r'–$\frac{1}{128}$', r'$\pm$0', r'+$\frac{1}{128}$', r'+$\frac{1}{64}$', r'+$\frac{1}{32}$'],
+            xlabel='Mean Tempo (BPM)', ylim=(-1/128 - 0.001, 1/32 + 0.0065),
+            ylabel='Mean piano asynchrony\n(proportional to measure)',
+
+        )
+        plt.setp(self.main_ax.spines.values(), linewidth=vutils.LINEWIDTH)
+        self.main_ax.axhline(0, 0, 1, lw=vutils.LINEWIDTH, ls=vutils.LINESTYLE, color=vutils.BLACK)
+        self.main_ax.tick_params(axis='both', bottom=True, right=True, width=vutils.TICKWIDTH)
+        self.main_ax.text(322, 0, r'$\pm$0', ha='right', va='center', clip_on=False, zorder=1000)
+        for pict, v in zip(self._add_notation_vals(), ['–', '+', '+', '+']):
+            self.main_ax.add_artist(pict)
+            self.main_ax.text(pict.xy[0] - 1, pict.xy[1], v, ha='right', va='center', clip_on=False, zorder=1000)
+
+    def _format_marginal_ax(self):
+        """Formats axis-level properties for marginal axis"""
+        # Remove correct spines from marginal axis
+        for spine, ax in zip(['left', "bottom"], self.marginal_ax.flatten()):
+            ax.spines[[spine, 'right', 'top']].set_visible(False)
+            ax.tick_params(axis='both', width=vutils.TICKWIDTH)
+            plt.setp(ax.spines.values(), linewidth=vutils.LINEWIDTH)
+        # Set other features for the main axis
+        self.marginal_ax[0].set(
+            xlabel='', ylabel='', yticks=[0], yticklabels=[''], xticklabels=[], xlim=(100, 315),
+            xticks=[100, 150, 200, 250, 300]
+        )
+        self.marginal_ax[1].set(
+            xlabel='', ylabel='', xticks=[0], xticklabels=[''], yticklabels=[], ylim=(-1/128 - 0.001, 1/32 + 0.0065),
+            yticks=[-1/128, 0, 1/128, 1/64, 1/32],
+        )
+
+    def _format_ax(self):
+        self._format_main_ax()
+        self._format_marginal_ax()
+
+    @staticmethod
+    def _add_notation_vals():
+        for val in [-128, 128, 64, 32]:
+            try:
+                img = plt.imread(fr'{utils.get_project_root()}\references\images\notation\notation_{abs(val)}.png')
+            except FileNotFoundError:
+                pass
+            # If we can get the image, then yield it to add to our plot
+            else:
+                yield mpl.offsetbox.AnnotationBbox(
+                    mpl.offsetbox.OffsetImage(img, clip_on=False, zoom=0.5), (320, 1/val),
+                    frameon=False, xycoords='data', clip_on=False, annotation_clip=False
+                 )
+
+    def _format_fig(self):
+        """Format figure-level properties"""
+        self.fig.subplots_adjust(left=0.075, right=0.99, top=0.99, bottom=0.09, hspace=0.1, wspace=0.1)
+
+
+if __name__ == '__main__':
+    pass
