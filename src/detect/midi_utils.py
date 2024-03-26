cd20f26163ebff31cf9463ec613c0a93475f1c5e
Huw Cheston <hwc31@cam.ac.uk>
2024-03-26 16:25:04 +0000

    Added melodic features and utilities for working with MIDI files

---
+++ src/detect/midi_utils.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Utility classes, functions, and variables when working with MIDI files."""
+
+from itertools import groupby
+from typing import Generator
+
+import numpy as np
+import pretty_midi
+
+from src.detect.detect_utils import FREQUENCY_BANDS, OnsetMaker
+
+
+class Note(pretty_midi.Note):
+    """Overrides `pretty_midi.Note` with a few additional properties"""
+
+    def __init__(self, note):
+        super().__init__(**note.__dict__)
+        self.ioi = super().duration
+        self.note = ''.join(i for i in pretty_midi.note_number_to_name(self.pitch) if not i.isdigit())
+        self.octave = int(''.join(i for i in pretty_midi.note_number_to_name(self.pitch) if i.isdigit()))
+        self.pitch_class = pretty_midi.note_name_to_number(f'{self.note}0') - 12
+
+    def __repr__(self):
+        return 'Note(start={:f}, end={:f}, ioi={:f}, pitch={}, velocity={}, note={}, octave={}, pitch_class={})'.format(
+            self.start, self.end, self.ioi, self.pitch, self.velocity, self.note, self.octave, self.pitch_class
+        )
+
+
+class MelodyMaker:
+    SHORTEST_RHYTHM = 1 / 64
+    TIME_THRESH = 0.01    # notes shorted than 10 milliseconds will be removed
+    NOTE_LB, NOTE_UB = (int(pretty_midi.hz_to_note_number(FREQUENCY_BANDS['piano'][fm])) for fm in ['fmin', 'fmax'])
+    MIDDLE_C = 60
+
+    def __init__(self, midi_fpath: str, om: OnsetMaker):
+        # Attributes taken directly from the `OnsetMaker` class
+        self.beats = om.summary_dict['beats']
+        self.downbeats = om.ons['downbeats_manual']
+        self.tempo = om.tempo
+        self.quarter_note = 60 / self.tempo    # duration of a quarter note in seconds
+        self.time_signature = om.item['time_signature']
+        self.tempo_thresh = (self.quarter_note * self.time_signature) / 1 / 64  # len(one bar) / (musical value)
+        self.midi = self.load_midi(midi_fpath)
+
+    def load_midi(self, midi_fpath) -> pretty_midi.Instrument:
+        # TODO: check we don't have more than one instrument here
+        return pretty_midi.PrettyMIDI(midi_fpath, initial_tempo=self.tempo).instruments[0]
+
+    def _remove_iois_below_threshold(self, notes: list[pretty_midi.Note]) -> list[pretty_midi.Note]:
+        ioi = lambda n: n.end - n.start
+        return sorted(
+            [n for n in notes if not any([
+                ioi(n) < self.tempo_thresh,
+                ioi(n) < self.TIME_THRESH
+            ])],
+            key=lambda n: n.start
+        )
+
+    def _remove_pitches_below_threshold(self, notes: list[pretty_midi.Note]) -> list[pretty_midi.Note]:
+        return sorted(
+            [n for n in notes if not any([
+                n.pitch <= self.NOTE_LB,
+                n.pitch >= self.NOTE_UB,
+                n.pitch <= self.MIDDLE_C
+            ])],
+            key=lambda n: n.start
+        )
+
+    @staticmethod
+    def _quantize_notes_in_beat(
+            beat1: float,
+            beat2: float,
+            notes: list[pretty_midi.Note],
+            num_ticks: int = 8
+    ) -> Generator[pretty_midi.Note, None, None]:
+        ticks = np.linspace(beat1, beat2, num_ticks)
+        closest = lambda n: ticks[np.argmin(np.abs(n.start - ticks))]
+        yield from (pretty_midi.Note(start=closest(n), end=n.end, pitch=n.pitch, velocity=n.velocity) for n in notes)
+
+    @staticmethod
+    def _extract_highest_note(
+            notes: list[pretty_midi.Note]
+    ) -> Generator[Note, None, None]:
+        for _, vals in groupby(sorted(notes, key=lambda x: x.start), lambda x: x.start):
+            yield Note(max(vals, key=lambda n: n.pitch))
+
+    def extract_melody(self):
+        notes = self._remove_pitches_below_threshold(self._remove_iois_below_threshold(self.midi.notes))
+        for beat1, beat2 in zip(self.beats, self.beats[1:]):
+            quantized_notes = self._quantize_notes_in_beat(beat1, beat2, [n for n in notes if beat1 <= n.start < beat2])
+            yield from self._extract_highest_note(quantized_notes)
+
+    def chunk_melody(
+            self,
+            melody_notes: list[Note],
+            chunk_measures: int = 4,
+            overlapping_chunks: bool = True
+    ) -> Generator[tuple[Note], None, None]:
+        if overlapping_chunks:
+            z = zip(self.downbeats, self.downbeats[chunk_measures:])
+        else:
+            z = zip(self.downbeats[::chunk_measures], self.downbeats[chunk_measures::chunk_measures])
+        chunks = []
+        for db1, db2 in z:
+            chunks.append(tuple(m for m in melody_notes if db1 <= m.start < db2))
+        return chunks
---
+++ src/features/melody_features.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Utility classes, functions, and variables used in extracting melodic features"""
+
+import math
+from collections import Counter
+
+import numpy as np
+
+from src import utils
+from src.detect.midi_utils import Note, MelodyMaker
+from src.features.features_utils import BaseExtractor
+
+__all__ = [
+    'MelodyChunkManager', 'PitchExtractor', 'IntervalExtractor', 'ContourExtractor', 'TonalityExtractor'
+]
+
+HURON_CONTOURS = dict(
+    # contour-name: function, taking in lowest note, mean inbetween notes, and highest note
+    convex=lambda p1, p2, pn: p1 < p2 > pn,
+    ascending_horizontal=lambda p1, p2, pn: p1 < p2 == pn,
+    ascending=lambda p1, p2, pn: p1 < p2 < pn,
+    horizontal=lambda p1, p2, pn: p1 == p2 == pn,
+    horizontal_descending=lambda p1, p2, pn: p1 == p2 > pn,
+    horizontal_ascending=lambda p1, p2, pn: p1 == p2 < pn,
+    descending_horizontal=lambda p1, p2, pn: p1 > p2 == pn,
+    descending=lambda p1, p2, pn: p1 > p2 > pn,
+    concave=lambda p1, p2, pn: p1 > p2 < pn
+)
+
+
+class MelodyChunkManager(BaseExtractor):
+    """For a given `MelodyMaker` instance, applies the given `extractor` to all chunks and averages the results"""
+
+    def __init__(self, extractor, mm: MelodyMaker, **kwargs):
+        super().__init__()
+        mel = list(mm.extract_melody())
+        chunk_list = [extractor(chunk, **kwargs).summary_dict for chunk in mm.chunk_melody(mel)]
+        chunk_dict = {k: [dic[k] for dic in chunk_list] for k in chunk_list[0]}
+        self.update_summary_dict(chunk_dict.keys(), chunk_dict.values())
+
+    def update_summary_dict(self, array_names, arrays, *args, **kwargs):
+        """Applies all the functions in `summary_funcs` to each array of values from the base `extractor`"""
+        for func_name, func in self.summary_funcs.items():
+            for array_name, array in zip(array_names, arrays):
+                try:
+                    self.summary_dict[f'{array_name}_{func_name}'] = func(array)
+                # TODO: integrate some method for getting features from categorical variables e.g. Huron contour
+                except TypeError:
+                    self.summary_dict[f'{array_name}_{func_name}'] = np.nan
+
+
+class PitchExtractor(BaseExtractor):
+    def __init__(self, my_notes: list[Note]):
+        super().__init__()
+        pitches = [i.pitch for i in my_notes]
+        pitch_classes = [i.pitch_class for i in my_notes]
+        self.summary_dict = {
+            'pitch_range': max(pitches) - min(pitches) if len(pitches) > 1 else np.nan,
+            'pitch_std': np.nanstd(pitches),
+            'pitch_class_entropy': normalized_entropy(pitch_classes, 24)
+        }
+
+
+class IntervalExtractor(BaseExtractor):
+    def __init__(self, my_notes: list[Note]):
+        super().__init__()
+        intervals = [i2.pitch - i1.pitch for i2, i1 in zip(my_notes, my_notes[1:])]
+        abs_intervals = [abs(i) for i in intervals]
+        self.summary_dict = dict(
+            abs_interval_range=max(abs_intervals) - min(abs_intervals) if len(abs_intervals) > 1 else np.nan,
+            mean_abs_interval=np.nanmean(abs_intervals),
+            std_abs_interval=np.nanstd(abs_intervals),
+            # Per Mullensiefen (2009), we use a different value for normalizing interval entropy here
+            interval_entropy=normalized_entropy(intervals, norm=23),
+            modal_interval=self.modal_interval(intervals)
+        )
+
+    @staticmethod
+    def modal_interval(intervals) -> int:
+        # Get the frequency of each interval
+        abs_freqs = Counter(intervals)
+        # Get the value of the most frequent intervals
+        most_frequent = max(abs_freqs.values())
+        # Get the numbers of the intervals that appeared most frequently
+        top_freqs = [k for k, v in abs_freqs.items() if v == most_frequent]
+        # Return the maximum interval: see Mullensiefen (2009)
+        return max(top_freqs, key=lambda i: abs(i))
+
+
+class ContourExtractor(BaseExtractor):
+    def __init__(self, my_notes: list[Note]):
+        super().__init__()
+        pitches = [i.pitch for i in my_notes]
+        self.summary_dict = dict(
+            huron_contour=self.huron_contour(pitches),
+        )
+
+    @staticmethod
+    def huron_contour(pitches: list[int]) -> str:
+        firstpitch, meanpitch, lastpitch = pitches[0], np.mean(pitches[1:-2]), pitches[-1]
+        for name, func in HURON_CONTOURS.items():
+            # As soon as we hit a function that returns True, break out of the loop to save time
+            if func(firstpitch, meanpitch, lastpitch):
+                return name
+
+
+class TonalityExtractor(BaseExtractor):
+    # Krumhansl-Schmuckler weights for major and minor keys
+    MAJ_PROFILE = [6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88]
+    MIN_PROFILE = [6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]
+
+    def __init__(self, my_notes: list[Note]):
+        super().__init__()
+        notes = [i.note for i in my_notes]
+        self.corrs = self.krumhansl_schmuckler(notes)
+        # Remove any duplicate correlations, sort in ascending order, and get the largest positive correlation
+        corrs_set = sorted(set(self.corrs.values()))
+        largest_corr = corrs_set[-1]
+        self.summary_dict = dict(
+            # Simply the largest correlation
+            tonalness=largest_corr,
+            # The largest correlation divided by the second-largest correlation
+            tonal_clarity=largest_corr / corrs_set[-2],
+            # The largest correlation divided by the sum of all positive correlations
+            tonal_spike=largest_corr / sum(i for i in corrs_set if i > 0),
+            mode=[c for c in self.corrs.keys() if self.corrs[c] == largest_corr][0].split(' ')[-1]
+        )
+
+    def krumhansl_schmuckler(self, notes: list[str]) -> dict:
+        """
+        Implements the Krumhansl-Schmuckler key-finding algorithm.
+
+        Returns a dictionary of tonalities and corresponding correlation coefficients for a given list of `notes`.
+        """
+        len_pitches = len(utils.ALL_PITCHES)
+        # Calculate the relative frequency of each note in the input
+        relfreqs = {k: v / len(notes) for k, v in Counter(notes).items()}
+        # Sort the relative frequencies by the standard C-B pitch class
+        keyfreqs = {k: relfreqs[k] if k in relfreqs.keys() else 0 for k in utils.ALL_PITCHES}
+        corrs = {}
+        # Iterate through each note
+        for i in range(len_pitches):
+            key_test = [keyfreqs.get(utils.ALL_PITCHES[(i + m) % len_pitches]) for m in range(len_pitches)]
+            # Calculate the correlation coefficient against the major and minor profiles for this note
+            corrs[utils.ALL_PITCHES[i] + ' major'] = np.corrcoef(self.MAJ_PROFILE, key_test)[1, 0]
+            corrs[utils.ALL_PITCHES[i] + ' minor'] = np.corrcoef(self.MIN_PROFILE, key_test)[1, 0]
+        return corrs
+
+
+def normalized_entropy(
+        array: list[int],
+        norm: int
+) -> float:
+    """Calculates the entropy of a given `array` and normalizes by `log2(norm)`, per Mulliensiefen (2009)"""
+    # Calculate the relative frequency (fi) of each pitch class in the phrase
+    #  i.e., the number of occurrences of the given class, divided by the number of occurrences of all classes
+    rel_freq = [v / len(array) for _, v in Counter(array).items()]
+    # Calculate the pitch class entropy
+    #  Equivalent to the sum of fi * log2(fi) for fi in rel_freq,
+    #  normalised by the maximum entropy given an upper phrase length limit (defaults to 24 as in Mullensiefen (2009))
+    return -(sum(v * math.log(v, 2) for v in rel_freq) / math.log(norm, 2))
+
+
+if __name__ == '__main__':
+    fp = f'{utils.get_project_root()}\data\cambridge-jazz-trio-database-v01\corpus_chronology\evansb-ttttwelvetonetune-gomezemorellm-1971-360d7a67'
+    track = utils.load_track_from_files(fp)
+    maker = MelodyMaker(fp + '\piano_midi.mid', track)
+    features = [PitchExtractor, IntervalExtractor, TonalityExtractor, ContourExtractor]
+    mel_list = [MelodyChunkManager(feature, maker).summary_dict for feature in features]
+    mel_features = {k: v for d in mel_list for k, v in d.items()}
+    print(mel_features)
